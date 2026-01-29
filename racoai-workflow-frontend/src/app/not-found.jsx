048fb3654c24139c63a502c7c350bab64f11fb75
hriday33333 <zakirhasanhriday@gmail.com>
2026-01-29 16:12:22 +0600

    added not-found component

---
+++ racoai-workflow-frontend/src/app/not-found.jsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h2 className="text-3xl text-center font-bold">Not found</h2>
+    </div>
+  );
+};
+
+export default NotFound;
